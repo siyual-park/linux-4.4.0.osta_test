38a24215e83cafc1c6948292426fa5f6fb615d69
siyual-park <kdpark0723@gmail.com>
2021-12-03 16:39:58 +0900

    fix error

--- assignment3/test.c
+++ assignment3/test.c
@@ -5,14 +5,14 @@
 
 int main() {
     unsigned long max_mem;
-    scanf("%ld\n", &max_mem);
+    scanf("%ld", &max_mem);
     
     syscall(326, getpid(), max_mem);
 
     while (1) {
         unsigned long mem;
         printf("mem size: ");
-        scanf("%ld\n", &mem);
+        scanf("%ld", &mem);
 
         malloc(mem);
     }
