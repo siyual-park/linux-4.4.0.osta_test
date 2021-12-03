bdc6d700bd52bb6ac32461631b7f11701e400fcb
siyual-park <kdpark0723@gmail.com>
2021-12-03 16:33:04 +0900

    fix error

--- assignment3/test.c
+++ assignment3/test.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/syscall.h>
+#include <unistd.h>
 
 int main() {
-    int pid;
     unsigned long max_mem;
-    scanf("%d %ld\n", &pid, &max_mem);
+    scanf("%ld\n", &max_mem);
     
-    syscall(326, pid, max_mem);
+    syscall(326, getpid(), max_mem);
 
     while (1) {
         unsigned long mem;
